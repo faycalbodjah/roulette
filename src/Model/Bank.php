d684a78aa920939993257b4fae45b66948f17de5
Fayçal BORDJAH <fbordjah@linagora.com>
2019-02-13 17:09:50 +0100

    composer and first project structur

---
+++ src/Model/Bank.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+namespace App\Model;
+/**
+ *
+ */
+class Bank
+{
+  /** @var int*/
+  private $amount;
+  function __construct()
+  {
+
+  }
+  public function dispatch(){
+    
+  }
+}
+
+ ?>
